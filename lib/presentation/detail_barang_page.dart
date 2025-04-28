73c2b7dc663098a1e33e4b7d73459300d21acd87
Wahyu <firmansyahw503@gmail.com>
2025-04-29 05:59:18 +0700

    layout dan ikon visual untuk halaman detail barang

--- lib/presentation/detail_barang_page.dart
+++ lib/presentation/detail_barang_page.dart
@@ -16,3 +16,39 @@ class DetailBarangPage extends StatelessWidget {
     required this.jumlahBarang,
     required this.hargaSatuan,
   });
+  @override
+  Widget build(BuildContext context) {
+    final int totalHarga = jumlahBarang * hargaSatuan;
+    final currencyFormat = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp. ',
+      decimalDigits: 0,
+    );
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF8F8),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              const Spacer(),
+              Container(
+                padding: const EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green.withOpacity(0.1),
+                ),
+                child: const Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                  size: 130,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
