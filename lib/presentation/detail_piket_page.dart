d79117d2a064f0ee442ae3a77ed0b646aa98a2ea
Wahyu <firmansyahw503@gmail.com>
2025-04-29 05:04:37 +0700

    struktur awal DetailPiketPage dengan parameter nama, tanggal, dan tugas

---
+++ lib/presentation/detail_piket_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String nama;
+  final String tanggal;
+  final String tugas;
+
+  const DetailPiketPage({
+    super.key,
+    required this.nama,
+    required this.tanggal,
+    required this.tugas,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+    );
+  }
+}
