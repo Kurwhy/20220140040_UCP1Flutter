6048a2177de84913070afe36ca90935ae5310ad2
Wahyu <firmansyahw503@gmail.com>
2025-04-29 05:53:49 +0700

    DetailBarangPage dengan parameter input yang diperlukan

---
+++ lib/presentation/detail_barang_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DetailBarangPage extends StatelessWidget {
+  final DateTime tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlahBarang;
+  final int hargaSatuan;
+
+  const DetailBarangPage({
+    super.key,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlahBarang,
+    required this.hargaSatuan,
+  });
