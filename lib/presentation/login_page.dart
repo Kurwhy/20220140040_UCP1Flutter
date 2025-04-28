1263caa077554fa17c2d638eb964a841c4ce4f50
Wahyu <firmansyahw503@gmail.com>
2025-04-28 22:26:09 +0700

    Menambah gambar dan text

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -16,8 +16,27 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: const Color(0xFFFEF7F5),
       body: Form(
         key: _formKey,
-        child: const Center(
-          child: Text("Login Page"),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: ListView(
+            children: [
+              const SizedBox(height: 30),
+              Center(
+                child: Image.asset('assets/images/cimory.png', height: 200),
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  "SELAMAT DATANG KEMBALI",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
