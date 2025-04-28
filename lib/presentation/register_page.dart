e5adb10bb484854c6f28adc4c8f54628ca6649b1
Wahyu <firmansyahw503@gmail.com>
2025-04-28 22:51:41 +0700

    tambah field input nama

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -15,6 +15,7 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
+  final _namaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +37,33 @@ class _RegisterPageState extends State<RegisterPage> {
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 30),
+                const Text(
+                  'Nama Lengkap',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: TextFormField(
+                    controller: _namaController,
+                    decoration: InputDecoration(
+                      labelText: 'Nama Lengkap',
+                      prefixIcon: const Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFF343A7C)),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nama lengkap tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
               ],
             ),
           ),
